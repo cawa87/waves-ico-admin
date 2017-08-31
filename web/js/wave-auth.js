818030077b16d667c35667b54e24d2ea6f5945d5
behemothoz <behemothoz@yandex.ru>
2017-08-31 21:35:01 +0300

    form waves

--- web/js/wave-auth.js
+++ web/js/wave-auth.js
@@ -2,15 +2,15 @@
 
     var wWaves = $('.alert-waves'),
         wInput = wWaves.find('.waves-input');
-        // wBtn = wWaves.find('.waves-btn'),
+        wBtn = wWaves.find('.waves-btn');
         // wOvwerlay = $('.waves-overlay');
 
     var wTip = wWaves.find('.waves-tip');
 
 
-    $('#attach_address_save').on('click', function(e) {
-
+    wBtn.on('click', function(e) {
         e.preventDefault();
+
         var url = Routing.generate('attach_user_address');
         var formSerialize = $('.waves-form').serialize();
 
@@ -18,6 +18,7 @@
             //your callback here
             console.log(response);
         }, 'JSON');
+
     });
 
 })();
