8aff4b2dffe6465774cca99ba31915909384e42b
behemothoz <behemothoz@yandex.ru>
2017-09-05 20:19:26 +0300

    timeline

--- web/js/main.js
+++ web/js/main.js
@@ -158,4 +158,91 @@
 
 })();
 
+
+// Timeline
+
+;(function() {
+    var ALL_DAY = 25; // всего дней
+    var day = 1; // старт
+    var $count = $('.left-day');
+    var count = parseInt($count.text()); //temp
+    var arr = []; // массив для объекта каждого временного блока
+
+    // Временная шкала
+    var bonusItem = $('.bonus-item .part-scale');
+
+    // Конструктор объектов ItemBonus
+    var ItemBonus = function(item, start, end) {
+      this.item = item;
+      this.dayStart = start;
+      this.dayEnd = end;
+    }
+
+    // Создаем экземпляры и помещаем в массив
+    for(var i = 0; i < bonusItem.length; i++) {
+        arr.push(new ItemBonus($(bonusItem[i]), 1, $(bonusItem[i]).data('day')))
+    }
+
+    // Увеличение временной шкалы
+    function getTimeLine(obj, day) {
+          var qqq = (day - obj.dayStart) + obj.dayEnd;
+          obj.item.css('width', ( qqq / obj.dayEnd )  * 100 + '%');
+    }
+
+    function fillWidth(array, index, obj) {
+        for(var i = 0; i < index; i++) {
+            array[i].item.css('width', '100%');
+        }
+    }
+
+    // Поиск нужного временного блока
+    function countingTime(day) {
+
+        if ( day >= 1 && day <= 2 ) {
+            arr[0].dayStart = 2;
+            getTimeLine(arr[0], day);
+        }
+
+        if ( day >= 3 && day <= 5 ) {
+            fillWidth(arr, 1, arr[1]);
+            arr[1].dayStart = 5;
+            getTimeLine(arr[1], day);
+        }
+
+        if ( day >= 6 && day <= 8 ) {
+            fillWidth(arr, 2, arr[2]);
+            arr[2].dayStart = 8;
+            getTimeLine(arr[2], day);
+        }
+
+        if ( day >= 9 && day <= 12 ) {
+            fillWidth(arr, 3, arr[3]);
+            arr[3].dayStart = 12;
+            getTimeLine(arr[3], day);
+        }
+
+        if ( day >= 13 && day <= 17 ) {
+            fillWidth(arr, 4, arr[4]);
+            arr[4].dayStart = 17;
+            getTimeLine(arr[4], day);
+        }
+
+        if ( day >= 18 && day <= 25 ) {
+            fillWidth(arr, 5, arr[5]);
+            arr[5].dayStart = 25;
+            getTimeLine(arr[5], day);
+        }
+    }
+
+
+    // Event
+    $count.on('click', function() {
+      day = (ALL_DAY + 1) - count;
+      countingTime(day);
+    });
+
+    $count.trigger('click');
+
+})();
+
 //# sourceMappingURL=main.js.map
