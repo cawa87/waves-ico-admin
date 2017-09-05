7cc3cc130465ad29cebd25120cbb5383bc162df7
Oleksandr Zamiatin <cawa123@mail.ru>
2017-09-05 19:57:44 +0300

    fix

--- src/AppBundle/Service/Twig/InvestmentsManagement.php
+++ src/AppBundle/Service/Twig/InvestmentsManagement.php
@@ -15,22 +15,43 @@ class InvestmentsManagement
     /**
      * UserManagement constructor.
      */
-    public function __construct(ObjectManager $em)
+    public function __construct(ObjectManager $em, $day)
     {
         $this->em = $em;
+        $this->day = $day;
     }
 
-
-    public function getBtcCount()
-    {
-        //@todo
-        return 0;
-    }
 
     public function getDaysBeforeEnd()
     {
-        //@todo
-        return 30;
+        return $this->day;
+    }
+
+    public function getBonus()
+    {
+        $bonus = 0;
+
+        if ($this->day >= 28) {
+            $bonus = 25;
+        }
+
+        if ($this->day <= 27 && $this->day >= 25) {
+            $bonus = 20;
+        }
+
+        if ($this->day <= 24 && $this->day >= 22) {
+            $bonus = 15;
+        }
+
+        if ($this->day <= 21 && $this->day >= 18) {
+            $bonus = 10;
+        }
+
+        if ($this->day <= 17 && $this->day >= 12) {
+            $bonus = 5;
+        }
+
+        return $bonus;
     }
 
 
