c1100c5103b53219361621588116f563bd24b676
Oleksandr Zamiatin <cawa123@mail.ru>
2017-09-06 01:27:57 +0300

    invest total

--- src/AppBundle/Controller/InvestController.php
+++ src/AppBundle/Controller/InvestController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\CurrencyRate;
+use AppBundle\Entity\Transaction;
 use AppBundle\Form\AttachAddressType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -24,8 +25,37 @@ class InvestController extends Controller
 
         return $this->json([
             'success' => true,
-            'amount' => ($rate * $value)/10,  // @todo PRICE!
+            'amount' => ($rate * $value) / 10,  // @todo PRICE!
             'bonus' => $this->get('app.wrappers.bonus_service')->getBonus(),
+        ]);
+    }
+
+    /**
+     * @Method({"GET"})
+     * @Route("invest/total" , name="investment_total", options={"expose"=true})
+     */
+    public function getTotalInvestAction()
+    {
+
+
+        $btcRate = $this->getDoctrine()
+            ->getRepository(CurrencyRate::class)->getLastRateByCurrency(4);
+
+        $invested = $this->getDoctrine()->getRepository(Transaction::class)->getInvested();
+        $inv['USD'] = 0;
+
+        foreach ($invested as $currency) {
+            $rate = $this->getDoctrine()
+                ->getRepository(CurrencyRate::class)->getLastRateByCurrency($currency['currency']);
+            $amount = $rate * $currency['amount'];
+            $inv['USD'] += $amount;
+
+            $investedAll[$currency['currency']] = $currency;
+        }
+
+        return $this->json([
+            'invested_usd' => $inv['USD'],
+            'invested_btc' => $inv['USD'] / $btcRate
         ]);
     }
 }
